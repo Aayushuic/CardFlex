9cb20a2e51e4e2bdc05c0b3fc81f55b01231c80b
Aayush Chauhan <aayushchauhan1916@gmail.com>
2025-01-01 13:24:29 +0530

    JSK V_3 fix after purchased bug as shown not purchased product yet

--- backend/Controller/Payment/PaymentStatus.js
+++ backend/Controller/Payment/PaymentStatus.js
@@ -1,33 +1,56 @@
 const Order = require("../../modals/order");
+const User = require("../../modals/user");
 
-exports.paymentStatus = async(req,res)=>{
-    try {
-        const {orderId,razorpay_order_id} = req.query;
-        
-        if(!orderId||!razorpay_order_id){
-            return res.status(400).json({success:false,message:"invalid request"})
-        }
-        
+exports.paymentStatus = async (req, res) => {
+  try {
+    const { orderId, razorpay_order_id } = req.query;
 
-        const order  = await Order.findOne({_id:orderId,razorpay_order_id});
-        
-
-        if(!order){
-            return res.status(400).json({success:false,message:"invalid request"})
-        }
-
-        if(order.paymentStatus=="successful"){
-            return res.status(200).json({success:true,message:"payment Successfull",razorpay_payment_id:order.razorpay_payment_id})
-        }else if(order.paymentStatus=="pending"){
-            return res.status(200).json({success:false,pending:true,message:"payment pending"})
-        }else{
-            return res.status(200).json({success:false,message:"payment failed"})
-        }
-
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({success:false,message:"server error"})
+    if (!orderId || !razorpay_order_id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "invalid request" });
     }
-}
+    const order = await Order.findOne({ _id: orderId, razorpay_order_id });
+    if (!order) {
+      return res
+        .status(400)
+        .json({ success: false, message: "invalid request" });
+    }
 
+    if (order.user) {
+      const user = await User.findById(order.user);
+      if (user) {
+        // Clear the user's cart
+        for (let i = 0; i < order.product.length; i++) {
+          user.purchasedItem.push(order.product[i]._id);
+        }
+        user.cart = [];
+        if (user.isFirst) {
+          user.isFirst = false;
+        }
+        await user.save();
+      }
+    }
+
+    if (order.paymentStatus == "successful") {
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: "payment Successfull",
+          razorpay_payment_id: order.razorpay_payment_id,
+        });
+    } else if (order.paymentStatus == "pending") {
+      return res
+        .status(200)
+        .json({ success: false, pending: true, message: "payment pending" });
+    } else {
+      return res
+        .status(200)
+        .json({ success: false, message: "payment failed" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: "server error" });
+  }
+};
